4aa2055536e0061da97a65650e8fa0864689922f
ssemambo abdulhakim <harrisonssemambo1@gmail.com>
2026-06-22 04:27:21 -0700

    add complete_task with error handling

--- tracker.py
+++ tracker.py
@@ -7,3 +7,11 @@ def add_task (title):
 if __name__ == "__main__":
     add_task("buy milk")
     print(tasks)
+
+def complete_task(title):
+    for task in tasks :
+        if task('title') == title:
+            task ['done'] = True
+            print(f'completed:{title}') 
+            return
+        raise ValueError(f'task not found{title}')        
